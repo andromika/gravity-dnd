1e794aa810d6f9fac3005ca7a5744c28b02944b9
Flicky <flicky@pollux.gg>
2026-03-18 01:25:06 -0300

    create dnd demo

---
+++ src/components/DragAndDrop/DragAndDrop.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.st-profile-medalpicker2-dnd(@dragstart.prevent)
+  slot(:dragState="dragState" :cancelDrag="cancelDrag")
+  DragPreviewOverlay(
+    :visible="dragState.active && Boolean(dragState.item)"
+    :item="dragState.item"
+    :x="dragState.clientX"
+    :y="dragState.clientY"
+    :shiftX="dragState.shiftX"
+    :shiftY="dragState.shiftY"
+    :width="dragState.previewWidth"
+    :height="dragState.previewHeight"
+  )
+    slot(name="preview" :item="dragState.item")
+</template>
+
+<script setup lang="ts">
+import DragPreviewOverlay from '../Draggable/DragPreviewOverlay.vue';
+import { provideDragDropContext } from '../Draggable/useDragDropContext';
+import './DragAndDrop.scss';
+
+const { dragState, cancelDrag } = provideDragDropContext();
+</script>
