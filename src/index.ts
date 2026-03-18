a7558dd1f3e5dff919e10826ce30a716567a734c
Flicky <flicky@pollux.gg>
2026-03-18 01:59:44 -0300

    create exports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,19 @@
-import Draggable from './components/Draggable.vue';
+export { default as GravityProvider } from './components/Draggable/DragDropProvider.vue';
+export { default as GravityDraggable } from './components/Draggable/Draggable.vue';
+export { default as GravitySlot } from './components/Slot/Slot.vue';
+export { default as GravityPool } from './components/Pool/Pool.vue';
 
-export { Draggable };
-
-export default {
-  install(app: any) {
-    app.component('GravityDraggable', Draggable);
-  }
-};
+export type {
+  GravityBoundary,
+  GravityCanDragContext,
+  GravityContainerKind,
+  GravityDropEvent,
+  GravityDropTarget,
+  GravityDraggableDropEvent,
+  GravityHoverTarget,
+  GravityMode,
+  GravityPoolReceiveEvent,
+  GravityPoolReorderEvent,
+  GravitySlotDropEvent,
+  GravitySource,
+} from './components/Draggable/contracts';
