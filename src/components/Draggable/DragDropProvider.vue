f18beecb84510b8ca105ff37866367d0bcf96ffa
Flicky <flicky@pollux.gg>
2026-03-18 01:24:10 -0300

    separate drag provider

---
+++ src/components/Draggable/DragDropProvider.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+.st-gravity-provider(@dragstart.prevent)
+  slot(:dragState="dragState" :cancelDrag="cancelDrag")
+</template>
+
+<script setup lang="ts">
+import { provideDragDropContext } from './useDragDropContext';
+import './DragDropProvider.scss';
+
+const { dragState, cancelDrag } = provideDragDropContext();
+</script>
